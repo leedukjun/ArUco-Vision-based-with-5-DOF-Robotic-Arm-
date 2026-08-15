58e0f919691837f9b68ec2a015817b8a5073e95e
trunglda22225-pixel <trungld.a2.2225@gmail.com>
2026-08-15 16:18:54 +0700

    update PCA9685

--- servo/servo.ino
+++ servo/servo.ino
@@ -1,14 +1,20 @@
-#include <Servo.h>
-
-Servo myServo;
+#include <Wire.h>
+#include <Adafruit_PWMServoDriver.h>
+Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver();
 
 void setup() {
   Serial.begin(9600);
-  myServo.attach(9); 
+  pwm.begin();
+  pwm.setOscillatorFrequency(27000000); 
+  pwm.setPWMFreq(50);  
 }
-
 void loop() {
   if (Serial.available()) {
-    myServo.write(Serial.readStringUntil('\n').toInt());
+
+    int targetAngle = Serial.readStringUntil('\n').toInt(); 
+
+    int pulse = map(targetAngle, 0, 180, 150, 600);
+  
+    pwm.setPWM(0, 0, pulse); 
   }
 }
